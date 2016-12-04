04b9d0a376b1bba8a3dd2c4b1ac3344fe569a9b9
Alexandru Coman <Alex@Aurass-MacBook-Pro.local>
2016-12-04 10:19:20 +0100

    persistance for user updates

--- GoPadada/app/src/main/java/padada/com/managers/SharedPrefsManager.java
+++ GoPadada/app/src/main/java/padada/com/managers/SharedPrefsManager.java
@@ -9,6 +9,8 @@ import padada.com.model.Customer;
 public class SharedPrefsManager {
 
 	private static final String KEY_CUSTOMER_ID = "keyCustomerId";
+	private static final String KEY_CUSTOMER_LEVEL = "keyCustomerLevel";
+	private static final String KEY_CUSTOMER_POINTS = "keyCustomerPoints";
 
 	private SharedPreferences mSharedPreferences;
 
@@ -18,12 +20,16 @@ public class SharedPrefsManager {
 
 	public void saveCustomer(Customer customer) {
 		mSharedPreferences.edit().putString(KEY_CUSTOMER_ID, customer.getObjectId()).commit();
+		mSharedPreferences.edit().putInt(KEY_CUSTOMER_LEVEL, customer.getLevel()).commit();
+		mSharedPreferences.edit().putInt(KEY_CUSTOMER_POINTS, customer.getPoints()).commit();
 	}
 
 	public Customer getCustomer() {
 		if(mSharedPreferences.contains(KEY_CUSTOMER_ID)) {
 			return new Customer(
-					mSharedPreferences.getString(KEY_CUSTOMER_ID, null)
+					mSharedPreferences.getString(KEY_CUSTOMER_ID, null),
+					mSharedPreferences.getInt(KEY_CUSTOMER_LEVEL, 1),
+					mSharedPreferences.getInt(KEY_CUSTOMER_LEVEL, 1)
 			);
 		} else {
 			return null;
