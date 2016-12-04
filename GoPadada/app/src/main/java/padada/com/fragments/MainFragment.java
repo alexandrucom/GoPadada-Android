d8d49336f0dc9201c956385e5f991b897a184f37
vladmarusca <marusca04@gmail.com>
2016-12-04 09:00:42 +0100

    Header profile UI

--- GoPadada/app/src/main/java/padada/com/fragments/MainFragment.java
+++ GoPadada/app/src/main/java/padada/com/fragments/MainFragment.java
@@ -2,16 +2,17 @@ package padada.com.fragments;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import de.hdodenhof.circleimageview.CircleImageView;
 import padada.com.R;
 
 public class MainFragment extends Fragment {
-
-
+	private CircleImageView mCiProfile;
 	public MainFragment() {
 		// Required empty public constructor
 	}
@@ -23,4 +24,13 @@ public class MainFragment extends Fragment {
 		return inflater.inflate(R.layout.fragment_main, container, false);
 	}
 
+	@Override
+	public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+		super.onViewCreated(view, savedInstanceState);
+		initViews(view);
+	}
+
+	private void initViews(View view) {
+		mCiProfile = (CircleImageView) view.findViewById(R.id.ci_profile);
+	}
 }
