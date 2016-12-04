514ca91a6960572b57287db3291c17ccbf44b5b2
vladmarusca <marusca04@gmail.com>
2016-12-04 03:10:51 +0100

    Refactoring ride track

--- GoPadada/app/src/main/java/padada/com/fragments/BeaconFragment.java
+++ GoPadada/app/src/main/java/padada/com/fragments/BeaconFragment.java
@@ -3,7 +3,6 @@ package padada.com.fragments;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,35 +10,20 @@ import android.widget.ListView;
 
 import com.onyxbeacon.listeners.OnyxBeaconsListener;
 import com.onyxbeaconservice.Beacon;
-import com.onyxbeaconservice.IBeacon;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import padada.com.dal.ApiResult;
-import padada.com.managers.RideManager;
-import padada.com.model.Ride;
 import padada.com.receivers.ContentReceiver;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 
 public class BeaconFragment extends Fragment implements OnyxBeaconsListener {
-    private static final int TRANSPORT = 64875;
-    private static final int STATION = 30242;
-    private int countTransport;
-    private int countStation;
+
     // UI Elements
     private ListView mListView;
     private Activity mFragmentActivity;
     public BeaconAdapter mBeaconAdapter;
     private ContentReceiver mContentReceiver;
-    private long tStart = 0;
-    private long tEnd = 0;
-    private List<IBeacon> beaconList;
-    private boolean enterBus;
-    private RideManager mRideManager;
+    private RideHandler mRideHandler;
 
     public BeaconFragment() {
         // Required empty public constructor
@@ -60,11 +44,11 @@ public class BeaconFragment extends Fragment implements OnyxBeaconsListener {
     @Override
     public void onViewCreated(View v, Bundle savedInstanceState) {
         super.onViewCreated(v, savedInstanceState);
-        beaconList = new ArrayList<>();
+
         // Initialize content receiver. The SDK was initialized in MainActivity
         if (mContentReceiver == null) mContentReceiver = ContentReceiver.getInstance();
         mContentReceiver.setOnyxBeaconsListener(this);
-        mRideManager = new RideManager(getContext());
+        mRideHandler = new RideHandler(getContext());
     }
 
     public void onResume() {
@@ -79,74 +63,9 @@ public class BeaconFragment extends Fragment implements OnyxBeaconsListener {
         super.onDestroy();
     }
 
-    private static final String TAG = "BeaconFragment";
-
     @Override
     public void didRangeBeaconsInRegion(final List<Beacon> beacons) {
-        if (beacons.get(0) instanceof IBeacon) {
-            if (beacons.size() >= 2) {
-                beaconList.add(((IBeacon) beacons.get(0)));
-                if (beacons.get(1) instanceof IBeacon) {
-                    beaconList.add(((IBeacon) beacons.get(1)));
-                }
-            } else {
-                beaconList.add(((IBeacon) beacons.get(0)));
-            }
-
-            if (beaconList.size() > 7) {
-                for (IBeacon beacon : beaconList) {
-                    if (TRANSPORT == beacon.getMinor()) {
-                        countTransport += 1;
-                    }
-                    if (STATION == beacon.getMinor()) {
-                        countStation += 1;
-                    }
-                }
-
-                Log.d(TAG, "countTransport: " + countTransport);
-                Log.d(TAG, "countStation: " + countStation);
-                if (countStation == 0) {
-                    mRideManager.startRide(new Callback<ApiResult<Ride>>() {
-                        @Override
-                        public void success(ApiResult<Ride> rideApiResult, Response response) {
-                            Log.d(TAG, "success: enter" + rideApiResult.getResult().getStartedAt());
-                        }
-
-                        @Override
-                        public void failure(RetrofitError error) {
-                            Log.d(TAG, "failure: enter");
-                        }
-                    });
-                    Log.d(TAG, "enter bus: ");
-                    enterBus = true;
-                    tStart = System.currentTimeMillis();
-                }
-                if (enterBus && countStation > 0) {
-                    enterBus =false;
-                    Log.d(TAG, "exit bus: ");
-                    mRideManager.endRide(new Callback<ApiResult<Ride>>() {
-                        @Override
-                        public void success(ApiResult<Ride> rideApiResult, Response response) {
-                            Log.d(TAG, "success: exit " + rideApiResult.getResult().getEndedAt());
-                        }
-
-                        @Override
-                        public void failure(RetrofitError error) {
-                            Log.d(TAG, "failure: exit");
-                        }
-                    });
-                }
-                if (countTransport > 6) {
-                    int seconds = (int) ((tStart / 1000) % 60);
-                    Log.d(TAG, "time: " + seconds);
-                    tStart = 0;
-                }
-
-                beaconList = new ArrayList<>();
-                countTransport = 0;
-                countStation = 0;
-            }
-        }
+        mRideHandler.rideHandler(beacons);
         Activity currentActivity = getActivity();
         if (currentActivity != null) {
             currentActivity.runOnUiThread(new Runnable() {
---
+++ GoPadada/app/src/main/java/padada/com/fragments/RideHandler.java
@@ -0,0 +1,105 @@
+package padada.com.fragments;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.onyxbeaconservice.Beacon;
+import com.onyxbeaconservice.IBeacon;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import padada.com.dal.ApiResult;
+import padada.com.managers.RideManager;
+import padada.com.model.Ride;
+import retrofit.Callback;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+/**
+ * Created by Vlad on 04.12.2016.
+ */
+
+public class RideHandler {
+    private static final int TRANSPORT = 64875;
+    private static final int STATION = 30242;
+    private int countTransport;
+    private int countStation;
+    private long tStart = 0;
+    private List<IBeacon> beaconList;
+    private boolean enterBus;
+    private RideManager mRideManager;
+
+    public RideHandler(Context context) {
+        beaconList = new ArrayList<>();
+        mRideManager = new RideManager(context);
+    }
+
+    public void rideHandler(List<Beacon> beacons) {
+        if (beacons.get(0) instanceof IBeacon) {
+            if (beacons.size() >= 2) {
+                beaconList.add(((IBeacon) beacons.get(0)));
+                if (beacons.get(1) instanceof IBeacon) {
+                    beaconList.add(((IBeacon) beacons.get(1)));
+                }
+            } else {
+                beaconList.add(((IBeacon) beacons.get(0)));
+            }
+
+            if (beaconList.size() > 7) {
+                for (IBeacon beacon : beaconList) {
+                    if (TRANSPORT == beacon.getMinor()) {
+                        countTransport += 1;
+                    }
+                    if (STATION == beacon.getMinor()) {
+                        countStation += 1;
+                    }
+                }
+
+                Log.d(TAG, "countTransport: " + countTransport);
+                Log.d(TAG, "countStation: " + countStation);
+                if (countStation == 0) {
+                    mRideManager.startRide(new Callback<ApiResult<Ride>>() {
+                        @Override
+                        public void success(ApiResult<Ride> rideApiResult, Response response) {
+                            Log.d(TAG, "success: enter" + rideApiResult.getResult().getStartedAt());
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            Log.d(TAG, "failure: enter");
+                        }
+                    });
+                    Log.d(TAG, "enter bus: ");
+                    enterBus = true;
+                    tStart = System.currentTimeMillis();
+                }
+                if (enterBus && countStation > 0) {
+                    enterBus =false;
+                    Log.d(TAG, "exit bus: ");
+                    mRideManager.endRide(new Callback<ApiResult<Ride>>() {
+                        @Override
+                        public void success(ApiResult<Ride> rideApiResult, Response response) {
+                            Log.d(TAG, "success: exit " + rideApiResult.getResult().getEndedAt());
+                        }
+
+                        @Override
+                        public void failure(RetrofitError error) {
+                            Log.d(TAG, "failure: exit");
+                        }
+                    });
+                }
+                if (countTransport > 6) {
+                    int seconds = (int) ((tStart / 1000) % 60);
+                    Log.d(TAG, "time: " + seconds);
+                    tStart = 0;
+                }
+
+                beaconList = new ArrayList<>();
+                countTransport = 0;
+                countStation = 0;
+            }
+        }
+    }
+    private static final String TAG = "RideHandler";
+}
